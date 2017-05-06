d1dc9ebc96bc4f1dc77fbf3e2738dadbbac853f1
Andrew Cherkashyn <cherkashin.andrey90@gmail.com>
2017-05-06 11:03:26 -0700

    Phrase Placeholders and Test Script

---
+++ Assets/Scripts/PhrasesPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhrasesPanel : MonoBehaviour {
+
+	public List<GameObject> phrasesPlaceholders;
+
+	// Use this for initialization
+	void Start () {
+		updateWithPhrases(new string[] {
+			"Clean Our Filthy Toilets",
+			"Cover Today's Top Issues",
+			"Fake News",
+			"Skeletor",
+			"training dancing circus bears",
+			"reporting with top-tier newspapers",
+			"winning hot-dog eating contests"
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateWithPhrases(string[] phrases) {
+		for (int phraseIndex = 0; phraseIndex < phrasesPlaceholders.Count; phraseIndex++) {
+			phrasesPlaceholders[phraseIndex].SetActive(phraseIndex < phrases.Length);
+			if (phraseIndex < phrases.Length) {
+				phrasesPlaceholders[phraseIndex].GetComponentInChildren<Text>().text = phrases[phraseIndex];
+
+			}
+		}
+	}
+}
