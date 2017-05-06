026ba352613d0feda6e9ca2e45d8f4930443be78
Andrew Cherkashyn <cherkashin.andrey90@gmail.com>
2017-05-06 12:08:47 -0700

    Simple Drag’n’Drop mechanics

---
+++ Assets/Scripts/PhraseElement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PhraseElement : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public static GameObject itemBeingDragged;
+	Vector3 startPosition;
+	Vector3 startOffset;
+
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		itemBeingDragged = gameObject;
+		startPosition = transform.position;
+		startOffset = Input.mousePosition - transform.position;
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag(PointerEventData eventData) {
+		transform.position = Input.mousePosition - startOffset;
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag(PointerEventData eventData) {
+		itemBeingDragged = null;
+		transform.position = startPosition;
+	}
+
+	#endregion
+}
